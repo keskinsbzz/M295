7650a0395a28848e17eb14007f05041215c31fe8
Sudenaz61 <kesks@bzz.ch>
2025-06-03 16:01:07 +0200

    maven setup

---
+++ src/test/java/ch/flughafen/flugapi295/Flugapi295ApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.flughafen.flugapi295;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Flugapi295ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
